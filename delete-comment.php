8f2ccf8bb6de34c672f38de367038b6f2211b157
KHELIFA Yassine <prostam27@gmail.com>
2022-05-08 19:06:08 +0200

    REFACTORISATION DES REQUÊTES SQL

--- delete-comment.php
+++ delete-comment.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * DANS CE FICHIER ON CHERCHE A SUPPRIMER LE COMMENTAIRE DONT L'ID EST PASSE EN PARAMETRE GET !
  * 
@@ -8,7 +7,6 @@
  */
 require_once("libraries/database.php");
 require_once("libraries/utils.php");
-
 
 /**
  * 1. Récupération du paramètre "id" en GET
@@ -19,42 +17,25 @@ if (empty($_GET['id']) || !ctype_digit($_GET['id'])) {
 
 $id = $_GET['id'];
 
-
-/**
- * 2. Connexion à la base de données avec PDO
- * Attention, on précise ici deux options :
- * - Le mode d'erreur : le mode exception permet à PDO de nous prévenir violament quand on fait une connerie ;-)
- * - Le mode d'exploitation : FETCH_ASSOC veut dire qu'on exploitera les données sous la forme de tableaux associatifs
- * 
- * PS : Vous remarquez que ce sont les mêmes lignes que pour l'index.php ?!
- */
-$pdo = getPdo();
-
 /**
  * 3. Vérification de l'existence du commentaire
  */
-$query = $pdo->prepare('SELECT * FROM comments WHERE id = :id');
-$query->execute(['id' => $id]);
-if ($query->rowCount() === 0) {
+$commentaire = findOneComment($id);
+if (! $commentaire ) {
     die("Aucun commentaire n'a l'identifiant $id !");
 }
 
 /**
  * 4. Suppression réelle du commentaire
- * On récupère l'identifiant de l'article avant de supprimer le commentaire
+ * On récupère l'identifiant de l'article avant de supprimer le commentaire (pr: redirect)
  */
 
-$commentaire = $query->fetch();
 $article_id = $commentaire['article_id'];
 
-$query = $pdo->prepare('DELETE FROM comments WHERE id = :id');
-$query->execute(['id' => $id]);
+deleteOneComment($id);
 
 /**
  * 5. Redirection vers l'article en question
  */
 
 redirect("article.php?id=" . $article_id);
-
-// header("Location: article.php?id=" . $article_id);
-// exit();
